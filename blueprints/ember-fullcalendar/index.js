dca4af94cc593ad51c7dcc2f53707ca467220dc4
Chester Li <chester.li@panasonic.aero>
2016-05-05 11:55:53 -0700

    Stop using ES6 arrow functions

--- blueprints/ember-fullcalendar/index.js
+++ blueprints/ember-fullcalendar/index.js
@@ -2,8 +2,8 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('fullcalendar').then(() => {
-      return this.addBowerPackageToProject('fullcalendar-scheduler').then(() => {
+    return this.addBowerPackageToProject('fullcalendar').then(function() {
+      return this.addBowerPackageToProject('fullcalendar-scheduler').then(function() {
         return this.addAddonToProject('ember-cli-moment-shim', '0.6.2');
       });
     });
